9ece73b66fb80a8289d9f2093c82a3b3479684f7
github username <github email address>
2019-11-08 16:14:04 -0500

    adiallo pokemon

--- pokemon-teams-backend/app/controllers/trainers_controller.rb
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -4,6 +4,25 @@ class TrainersController < ApplicationController
       :include => {:pokemons => {:except => [:created_at, :updated_at]}
                   },
       :except => [:created_at, :updated_at]
+
+      #can also rewrite as
+      # trainerr=Trainer.all
+      # render json: trainers.to_json(serializeTrainer)
       )
   end
+
+
+  # can do serialize here if want to do differently
+
+  # private
+  # def serializeTrainer
+  # {
+          # :include => { :pokemons => {
+          #               :except => [:created_at, :updated_at]
+          #     }
+          # },
+          # :only =>[:name, :id]
+  # }
+  # end
+
 end
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -2,8 +2,10 @@ const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
 
+// DO DOM CONTENT LOADED HERE
 document.addEventListener("DOMContentLoaded", () => {
 
+//DO FETCH HERE TO GET THE TRAINERS
   fetch("http://localhost:3000/trainers")
   .then(response => response.json())
   .then(trainersArray => trainersArray.forEach(trainer => renderTrainer(trainer)))
@@ -11,7 +13,9 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 
-
+//SHOWING THE TRAINERS WITH DIV
+//FIND THE TRAINER CONTAINER TO APPEND. WHEN CREATING DIV MUST
+//APPEND TO CONTAINER
 function renderTrainer(trainer) {
   const container = document.querySelector("#trainer-container")
   const trainerDiv = document.createElement("div")
@@ -19,24 +23,28 @@ function renderTrainer(trainer) {
   trainerDiv.id = `trainer-${trainer.id}`
   container.appendChild(trainerDiv)
 
+  //PUTTING TRAINER NAME AND APPENDING TO DIV
   const trainerName = document.createElement("p")
   trainerDiv.appendChild(trainerName)
   trainerDiv.innerText = trainer.name
 
+  //CREATING A DOCUMENT ELEMENT AND ADDING EVENT LISTENER TO IT
   const button = document.createElement("button")
   trainerDiv.appendChild(button)
   button.innerText = "Add Pokemon"
   button.addEventListener("click", addPokemonToTeam)
 
+  //CREATING THE UL
   const pokemonUl = document.createElement("ul")
   trainerDiv.appendChild(pokemonUl)
 
+//GETTING THE POKEMONS OF THE TRAINER
   const pokemonsOfTrainer = trainer.pokemons
   pokemonsOfTrainer.forEach(pokemon => addPokemonLi(pokemon, pokemonUl))
 }
 
 
-
+//CALLBACK FUNCTION FOR ABOVE FUNCTION/ADDING POKEMONS
 function addPokemonLi(pokemon, pokemonUl) {
   const pokemonLi = document.createElement("li")
   pokemonUl.appendChild(pokemonLi)
@@ -51,7 +59,7 @@ function addPokemonLi(pokemon, pokemonUl) {
 }
 
 
-
+//EVENT LISTENER FOR ABOVE FUNCTION/DOING A FETCH FOR POST/CREATE
 function addPokemonToTeam(event) {
   const trainerId = parseInt(event.target.parentElement.id.split("-")[1], 10)
   const pokemonUl = event.target.parentElement.querySelector("ul")
@@ -71,7 +79,7 @@ function addPokemonToTeam(event) {
 }
 
 
-
+//DELETE POKEMEN FUNCTION WITH EVENT
 function deletePokemon(event) {
   const pokemonId = parseInt(event.target.id.split("-")[1], 10)
   fetch(`http://localhost:3000/pokemons/${pokemonId}`, {
@@ -79,11 +87,12 @@ function deletePokemon(event) {
     headers: {
       'Content-Type': 'application/json'
     }
-  })
+  })//THIS IS TO MANIPULATE THE DOM SO IT USER DOES NOT REFRESH
   .then(response => response.json())
   .then(pokemon => deletePokemonLi(pokemon))
 }
 
+//ACTUAL POKEMON DELITION THAT WAS A CALLBACK FROM ABOVE EVENT
 function deletePokemonLi(pokemon) {
   const pokemonLi = document.querySelector(`#pokemon-${pokemon.id}`).parentElement
   pokemonLi.remove()
